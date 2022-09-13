f0609310a16fbae37723a02fe576e26f9d0aaa0c
MOHD AIN <ainsaikhx7@gmail.com>
2022-09-13 21:39:28 +0530

    :fire:learn taking input via scanner

---
+++ FirstClass.java
@@ -0,0 +1,29 @@
+class FirstClass{
+    public static void main(String[] args) {
+        System.out.print("Hello Ain\n");
+        int a = 10;
+        int b = 23;
+        int perimeter = 2 * (a+b);
+        System.out.println("The perimeter is \n" + perimeter);
+
+
+        // Variables
+
+
+        String name = "Mohd Ain";
+        double price = 25.24;
+        int age = 23;
+        name = "Iron Man";
+        System.out.println(name  + price);
+
+        int mul = a * b;
+        System.out.println(mul);
+
+        // int []arr = new int[];
+        // arr[] = {12,12,21,221};
+        // System.out.println(arr);
+
+
+
+    }
+}
---
+++ takinginput.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class takinginput {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        // String name = sc.nextLine();
+
+
+        int sum = a+b;
+        System.out.println(sum);
+        // System.out.println(name);
+    }
+}
