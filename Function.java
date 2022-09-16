1d781e797cbb6a0f64cf3d868c65c18106dfe791
MOHD AIN <ainsaikhx7@gmail.com>
2022-09-16 20:27:09 +0530

    :rocket:Function

---
+++ Function.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class Function {
+    // public static void printMyName(String name){
+    //     System.out.println(name);
+    //     return;
+    // }
+
+
+    // public static int sum(int a, int b){
+    //     int sum = a+b;
+    //     System.out.println(sum);
+    //     return sum;
+    // }
+
+
+    // public static int calculateProduct(int a, int b){
+    //     return a * b;
+    // }
+
+        public static void printFactorial(int n){
+            if(n<0){
+                System.out.println("Invalid number");
+                return;
+            }
+            int factorial = 1;
+            for(int i=n; i>=1; i--){
+                factorial = factorial * i;
+            }
+
+            System.out.println(factorial);
+            return;
+        }
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        // for factorial
+
+        int n = scn.nextInt();
+
+        printFactorial(n);
+
+
+
+
+
+        // String name = scn.nextLine();
+        // int a = scn.nextInt();
+        // int b = scn.nextInt();
+
+        // System.out.println("The product is :"+ calculateProduct(a,b));
+        
+
+        // printMyName(name);  // function is called
+        // sum(a, b);
+    }
+}
