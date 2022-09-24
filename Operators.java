0849db73862cb061db913fe64020f4a498609fbe
MOHD AIN <ainsaikhx7@gmail.com>
2022-09-24 12:49:00 +0530

    :tada:operators

---
+++ Operators.java
@@ -0,0 +1,11 @@
+public class Operators {
+    public static void main(String[] args) {
+        
+
+        //Operators----> Symbols that tell compiler to perform some operation.
+
+        //Binary number system---->
+        // decimal to binary    
+        // 
+    }
+}
